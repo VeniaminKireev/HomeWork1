386182ea20c0bf0dfb01ba34973fb072e72caec0
VeniaminKireev <vakireev@edu.hse.ru>
2026-01-01 23:06:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
